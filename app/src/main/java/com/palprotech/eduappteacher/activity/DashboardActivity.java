4063cdd4c69a3bbf762c7c205a251aa09fadf945
Narendar <naren.happysanz@gmail.com>
2017-04-03 13:26:21 +0530

    Screen Design

---
+++ app/src/main/java/com/palprotech/eduappteacher/activity/DashboardActivity.java
@@ -0,0 +1,20 @@
+package com.palprotech.eduappteacher.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.palprotech.eduappteacher.R;
+
+/**
+ * Created by Narendar on 03/04/17.
+ */
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+
+}
