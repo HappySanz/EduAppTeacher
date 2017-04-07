9cdc4412fbef722e3eb06da5a42c5cf85303ff41
Narendar <naren.happysanz@gmail.com>
2017-04-07 19:14:00 +0530

    Screen Design

---
+++ app/src/main/java/com/palprotech/eduappteacher/activity/CreateTestHomework.java
@@ -0,0 +1,17 @@
+package com.palprotech.eduappteacher.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.palprotech.eduappteacher.R;
+
+/**
+ * Created by Narendar on 07/04/17.
+ */
+
+public class CreateTestHomework extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_test_homework);
+    }
+}
---
+++ app/src/main/java/com/palprotech/eduappteacher/activity/ProfileActivity.java
@@ -0,0 +1,20 @@
+package com.palprotech.eduappteacher.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.palprotech.eduappteacher.R;
+
+/**
+ * Created by Narendar on 05/04/17.
+ */
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_profile);
+    }
+
+}
